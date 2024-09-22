65ef9c979cf3175be541483b273d184ded341917
Reza Fathi <rezafathidev@gmail.com>
2024-09-22 14:38:51 +0300

    upgrade to symfony 7

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder():TreeBuilder
     {
         $treeBuilder = new TreeBuilder('vatri_google_drive');
 
--- src/DependencyInjection/VatriGoogleDriveExtension.php
+++ src/DependencyInjection/VatriGoogleDriveExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 class VatriGoogleDriveExtension extends Extension
 {
 	
-	public function load(array $configs, ContainerBuilder $container)
+	public function load(array $configs, ContainerBuilder $container):void
     {
 		// $configuration = new Configuration();
 
